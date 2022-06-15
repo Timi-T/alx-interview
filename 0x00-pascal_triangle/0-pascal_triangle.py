9d8e001931f43c48997f895cec91a14f2cfe5ee2
Timi-T <timiopebode@gmail.com>
2022-06-15 07:39:45 +0100

    Pascals triangle solution

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Implementation of pascal's triangle
+"""
+
+def pascal_triangle(n):
+    """
+    Function to return a list of lists representing each row of the triangle
+    """
+
+    #Creating a list and including the first row as a list
+    ret_list = [[1]]
+
+    #making an iteration for the number of rows for the triangle
+    for i in range(n - 1):
+        """
+        Making a temporary array that adds a zero to the beginning
+        and end of the last appended row
+        """
+        temp_arr = [0] + ret_list[-1] + [0]
+
+        row = []
+        #Iterating through the temporary array
+        for j in range(len(temp_arr) - 1):
+            row.append(temp_arr[j] + temp_arr[j + 1])
+        
+        #Append the new row to the list of lists
+        ret_list.append(row)
+    return ret_list
+
+print(pascal_triangle(6))
