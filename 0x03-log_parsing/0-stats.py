e5f1998be4adb6a700d4538379151bf854e8e9ef
Timi-T <timiopebode@gmail.com>
2022-07-05 23:18:15 +0100

    remove re module

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,7 +5,6 @@ The stdin filestream is read for inputs and metrics are
 computed by python
 """
 
-import re
 import sys
 
 
@@ -13,10 +12,24 @@ i = 1
 filesize = 0
 errors = ["200", "301", "400", "401", "403", "404", "405", "500"]
 error_count = {}
-line_regex = re.compile("ope")
+ip_re = "\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+         .\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+         .\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+         .\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+          - "
+datetime_regex = "[\b([1-9][0-9][0-9][0-9])\b-\b([0-9][0-9])\b\
+                  -\b([0-9][0-9])\b \b([0-9][0-9])\b:\b([0-9][0-9])\b:\b\
+                  ([0-9][0-9])\b.\b([0-9][0-9][0-9][0-9][0-9][0-9])\b] "
+url_regex = "\"GET /projects/260 HTTP/1.1\" "
+err_regex = "\b([2][0][0]|[3][0][1]|[4][0][0]|[4][0][1]|[4][0][3]|\
+             [4][0][4]|[4][0][5]|[5][0][0])\b "
+size_regex = "\b([1-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1][0][0-2][0-4])\b"
+#line_regex = re.compile(r''.format(ip_re + datetime_regex +
+                        #url_regex + err_regex + size_regex))
 try:
     for line in sys.stdin:
-        is_valid = line_regex.search(line)
+        #is_valid = line_regex.search(line)
+        is_valid = 1
         if is_valid:
             stats = line.split(' ')
             filesize += int(stats[-1])
