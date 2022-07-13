dda9b235b3f3111f87e34e99f1fbd7bede67f0cb
Timi-T <timiopebode@gmail.com>
2022-07-13 19:47:27 +0100

    retur false for 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,7 +12,7 @@ def validUTF8(data):
         if not isinstance(data[i], int):
             return False
         # When integer is not in the range of valid integers
-        if data[i] < 0 or data[i] >= 255:
+        if data[i] <= 0 or data[i] >= 255:
             return False
         # Convert each integer to its binary format
         bin_num = format(data[i], "b").zfill(8)
