fafe22a970bdeedd21bf0afd5bb5abc80c06981e
Timi-T <timiopebode@gmail.com>
2022-08-01 07:34:12 +0100

    module to rotate a 2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Rotate a 2d matrix
+"""
+
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]):
+    """Function to rotate a 2d matrix in place"""
+
+    n = len(matrix)
+    for i in range(n):
+        j = 0
+        while j + i < n:
+            temp = matrix[i][j + i]
+            matrix[i][j + i] = matrix[j + i][i]
+            matrix[j + i][i] = temp
+            j += 1
+
+    for i in range(n):
+        a = 0
+        b = n - 1
+        while a < b:
+            temp = matrix[i][a]
+            matrix[i][a] = matrix[i][b]
+            matrix[i][b] = temp
+            a += 1
+            b -= 1
+    return matrix
