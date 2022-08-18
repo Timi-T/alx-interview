9836dd71588966c5e15b4d48e549c9dd642615eb
Timi-T <timiopebode@gmail.com>
2022-08-18 19:34:45 +0100

    ilsand perimeter calculator

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Module to calculate island perimeter"""
+
+
+def island_perimeter(grid):
+    """Function to calculate island perimeter"""
+
+    perimeter = 0
+    i = 0
+    j = 0
+
+    for i in range(len(grid)):
+        sides = 0
+        row = grid[i]
+        for j in range(len(row)):
+            if row[j]:
+                if not j:
+                    sides += 1
+                if j == len(row) - 1:
+                    sides += 1
+                if not i:
+                    sides += 1
+                if i == len(grid) - 1:
+                    sides += 1
+                if j and not row[j - 1]:
+                    sides += 1
+                if i and not grid[i - 1][j]:
+                    sides += 1
+                if j != len(row) and not row[j + 1]:
+                    sides += 1
+                if i != len(grid) - 1 and not grid[i + 1][j]:
+                    sides += 1
+        perimeter += sides
+    return perimeter
