ceb6781b133d455768d0d73bb99ae67f0f7e660a
Timi-T <timiopebode@gmail.com>
2022-08-24 15:15:06 +0100

    when no numbers are passed

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -19,11 +19,8 @@ def isWinner(x, nums):
     Maria = 0
     Ben = 0
     for round in range(x):
-        empty = False
         turn = 0
         options = [n + 1 for n in range(nums[round])]
-        if not options:
-            empty = True
         i = 1
         while i < len(options):
             if isPrime(options[i]):
@@ -36,7 +33,7 @@ def isWinner(x, nums):
                     j += 1
                 turn += 1
             i += 1
-        if turn == 0 and not empty:
+        if turn == 0:
             Ben += 1
         elif turn % 2 == 0:
             Maria += 1
